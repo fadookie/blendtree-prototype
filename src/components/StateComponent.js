ce412f7b29811786a4cfb48aebb45f096bb2b021
Eliot Lash <fadookie@gmail.com>
2020-08-27 20:03:56 -0700

    Add temp StateComponent, refactor css loader to bundle rete style dependencies

---
+++ src/components/StateComponent.js
@@ -0,0 +1,32 @@
+import { Component, Input, Output } from "rete";
+import Socket from "../sockets";
+import NumControl from "../controls/NumControl";
+
+const DefaultControl = NumControl;
+
+export default class OutComponent extends Component {
+  constructor(CustomFieldControl) {
+    super("State");
+    this.CustomFieldControl = CustomFieldControl;
+  }
+
+  builder(node) {
+    var Field = this.CustomFieldControl || DefaultControl;
+    var inp1 = new Input("num1", "Number", Socket.num);
+    var out = new Output("num", "Number", Socket.num);
+
+    // inp1.addControl(new Field(this.editor, "num1", "number"));
+
+    return node
+      .addInput(inp1)
+      // .addControl(new Field(this.editor, "preview", "number", true))
+      .addOutput(out);
+  }
+
+  worker(node, inputs, outputs) {
+    // var n1 = inputs["num1"].length ? inputs["num1"][0] : node.data.num1;
+
+    // var n = this.editor.nodes.find((n) => n.id === node.id);
+    // if (n) n.controls.get("preview").setValue(n1);
+  }
+}
