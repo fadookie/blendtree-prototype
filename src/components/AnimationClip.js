2b92f5a2091c49ee8b1192628c5a41b78f04d288
Eliot Lash <fadookie@gmail.com>
2020-08-31 22:41:07 -0700

    bugfixes

--- src/components/AnimationClip.js
+++ src/components/AnimationClip.js
@@ -15,39 +15,19 @@ function AnimationClip() {
     this.loopWidget = this.addWidget("toggle","loop",this.properties.loop,"loop");
     // Using an ivar instead of a LGraphNode property so it doesn't get serialized
     this._playState = STATE_PLAYING;
-    this.playingWidget = this.addWidget("string", "playing", this._isPlaying);
+    this.playingWidget = this.addWidget("string", "playing", this._playState);
 }
 
 AnimationClip.title = "Animation Clip";
 AnimationClip.desc = "Animation Clip";
 AnimationClip.values = ["animator1", "animator2"];
 
-AnimationClip.prototype.setPlayState = function(newState) {
-  if (this.title === 'Animation Clip 2') 
-    console.log('@@@ setIsPlaying:', newState);
-
-  if (newState === this._playState) return;
-  // switch(this._playState) {
-  //   case STATE_PLAYING: {
-  //     break;
-  //   } case STATE_PAUSED: {
-  //     break;
-  //   } case STATE_STOPPED: {
-  //     break;
-  //   } default: {
-  //     throw new Error(`Unrecognized state: ${this._playState}`);
-  //   }
-  // }
-  if (newState === STATE_PLAYING && this._playState !== STATE_PAUSED) this._startTimeS = this.graph.globaltime;
-  this._playState = newState;
-  this.playingWidget.value = newState;
-  this.setDirtyCanvas(true, true);
-}
-
 AnimationClip.prototype.setValue = function(v)
 {
   this.setProperty("loop",v);
 }
+
+///#region LGraphNode Callbacks
 
 AnimationClip.prototype.onAction = function(action)
 {
@@ -77,7 +57,7 @@ AnimationClip.prototype.onExecute = function() {
     }
     const t = getT(seekTime * 1.5); //make triangle wave function loop
     const skeleton = getLerpedSkeleton(animators[this.properties.CLIP], t);
-    if (this.title === 'Animation Clip 2') 
+    if (this.title === 'Animation Clip 2' && seekTime < 0.1) 
       console.log(this.title, { loop, elapsedTimeS, seekTime, t, playState: this._playState }, skeleton); 
     // this._value = skeleton;
     this.setOutputData(0, skeleton);
@@ -87,6 +67,37 @@ AnimationClip.prototype.onDrawBackground = function(ctx) {
   //show the current value
   this.outputs[0].label = this.properties.CLIP;
 };
+
+ AnimationClip.prototype.onPropertyChanged = function(name, value) {
+  const widget = this.widgets.find(w => w.options.property == name);
+  if (widget) widget.value = value;
+  if (name === "loop" && !!value && this._playState === STATE_STOPPED) this.setPlayState(STATE_PLAYING);
+ };
+
+//#endregion LGraphNode Callbacks
+
+AnimationClip.prototype.setPlayState = function(newState) {
+  if (newState === this._playState) return;
+  // switch(this._playState) {
+  //   case STATE_PLAYING: {
+  //     break;
+  //   } case STATE_PAUSED: {
+  //     break;
+  //   } case STATE_STOPPED: {
+  //     break;
+  //   } default: {
+  //     throw new Error(`Unrecognized state: ${this._playState}`);
+  //   }
+  // }
+
+  if (this.title === 'Animation Clip 2')
+    console.log('@@@ setIsPlaying unique:', newState);
+
+  if (newState === STATE_PLAYING && this._playState !== STATE_PAUSED) this._startTimeS = this.graph.globaltime;
+  this._playState = newState;
+  this.playingWidget.value = newState;
+  this.setDirtyCanvas(true, true);
+}
 
 AnimationClip.prototype.pause = function(ctx) {
   if (this.title === 'Animation Clip 2') 
